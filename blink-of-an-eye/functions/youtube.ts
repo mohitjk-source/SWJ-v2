c0817444188081342cdea51737205337e4c84c7c
Mohit.jk <mohit.jk@gmail.com>
2025-12-01 18:55:18 +0000

    ts

---
+++ blink-of-an-eye/functions/youtube.ts
@@ -0,0 +1,18 @@
+export async function onRequest(context: EventContext<Env, any, any>) {
+  const apiKey = context.env.YOUTUBE_API_KEY;
+  const channelId = "UCWuk3YTnT5-9QxeoVF-wUFw"; // your channel ID
+
+  const res = await fetch(
+    `https://www.googleapis.com/youtube/v3/search?part=snippet&channelId=${channelId}&maxResults=5&key=${apiKey}`
+  );
+  const data = await res.json();
+
+  const items = (data.items || []).map((item: any) => ({
+    id: item.id.videoId,                        // flatten videoId
+    title: item.snippet.title,
+    thumb: item.snippet.thumbnails.medium.url,  // flatten thumbnail
+    publishedAt: item.snippet.publishedAt
+  }));
+
+  return Response.json(items); // return array directly
+}
